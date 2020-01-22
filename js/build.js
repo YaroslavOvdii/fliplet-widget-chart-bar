5c8767ae4aa24613e4d49bf9eb3a8148352691bb
Tony Wu <twu@fliplet.com>
2020-01-22 12:29:48 +0000

    Removes unnecessary return

--- js/build.js
+++ js/build.js
@@ -361,7 +361,7 @@
             }
           };
           // Create and save chart object
-          return Fliplet.Hooks.run('beforeChartRender', {
+          Fliplet.Hooks.run('beforeChartRender', {
             chartOptions: chartOpt,
             id: data.id,
             uuid: data.uuid,
